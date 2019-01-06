9e0c5c0307025d4976ca09962cdbb247398cf05c
hunter10 <hunter10@naver.com>
2019-01-06 21:18:11 +0900

    빠진 파일 추가

---
+++ Assets/Scripts/Arrive.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Arrive : AgentBehaviour 
+{
+	public float targetRadius;
+	public float slowRadius;
+	public float timeToTarget = 0.1f;
+
+	public override Steering GetSteering()
+	{
+		// 두 반경 변수들에 따른 대상과의 거리에 의존해 적합한 속도를 계산 
+		Steering steering = new Steering();
+		Vector3 direction = target.transform.position - transform.position;
+		float distance = direction.magnitude;
+		float targetSpeed;
+		if(distance < targetRadius)
+			return steering;
+		
+		if(distance > slowRadius)
+			targetSpeed = agent.maxSpeed;
+		else
+		{
+			targetSpeed = agent.maxSpeed * distance / slowRadius;
+		}
+
+		// steering	값을 설정하고, 최대 속도에 맞춰 값을 제한하는 코드 
+		Vector3 desiredVelocity = direction;
+		desiredVelocity.Normalize();
+		desiredVelocity *= targetSpeed;
+		steering.linear = desiredVelocity - agent.velocity;
+		steering.linear /= timeToTarget;
+		if(steering.linear.magnitude > agent.maxAccel) {
+			steering.linear.Normalize();
+			steering.linear *= agent.maxAccel;
+		}
+
+		return steering;
+	}
+}
---
+++ Assets/Scripts/Leave.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Leave : AgentBehaviour 
+{	
+	public float escapeRadius;
+	public float dangerRadius;
+	public float timeToTarget = 0.1f;
+
+	public override Steering GetSteering()
+	{
+		// 두 반경 변수들에 따른 대상과의 거리에 의존해 적합한 속도를 계산 
+		Steering steering = new Steering();
+		Vector3 direction = transform.position - target.transform.position;
+		float distance = direction.magnitude;
+		if(distance > dangerRadius)
+			return steering;
+		
+		float reduce;
+		if(distance < escapeRadius)
+			reduce = 0f;
+		else
+		{
+			reduce = distance / dangerRadius * agent.maxSpeed;
+		}
+		float targetSpeed = agent.maxSpeed - reduce;
+
+		// steering	값을 설정하고, 최대 속도에 맞춰 값을 제한하는 코드 
+		Vector3 desiredVelocity = direction;
+		desiredVelocity.Normalize();
+		desiredVelocity *= targetSpeed;
+		steering.linear = desiredVelocity - agent.velocity;
+		steering.linear /= timeToTarget;
+		if(steering.linear.magnitude > agent.maxAccel) {
+			steering.linear.Normalize();
+			steering.linear *= agent.maxAccel;
+		}
+
+		return steering;
+	}
+}
