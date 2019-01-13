0d6d9804d16cd595516171a97d0a7822cb6473f4
DESKTOP-4TAOHB8\hunter <hunter10@naver.com>
2019-01-14 01:31:59 +0900

    패스처리 추가

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour {
+
+    public MeshCollider ColliderForRayCast;
+    public Transform Player;
+    public Transform target;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hitInfo;
+            //ColliderForRayCast.Raycast(ray, out hitInfo, 100f);
+
+            //Ease easeX = Ease.Linear;
+            //Player.DOMove(target.position, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/PathFollower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using my;
+public class PathFollower : Seek
+{
+    public my.Path path;
+    public float pathOffset = 0.0f;
+    float currentParam;
+
+    public override void Awake()
+    {
+        base.Awake();
+        target = new GameObject();
+        currentParam = 0f;
+    }
+
+    public override Steering GetSteering()
+    {
+        currentParam = path.GetParam(transform.position, currentParam);
+        float targetParam = currentParam + pathOffset;
+        target.transform.position = path.GetPosition(targetParam);
+
+        return base.GetSteering();
+    }
+}
