8976194753b3b43aa1925fc353d37b88615226a1
hunter10 <hunter10@naver.com>
2019-02-09 11:05:59 +0900

    테스트

--- Assets/Scripts/LockonLaserMotion.cs
+++ Assets/Scripts/LockonLaserMotion.cs
@@ -6,6 +6,7 @@ public class LockonLaserMotion : MonoBehaviour {
 
     float speed = 0.1f;
     public Transform guidePrefab; // 인간이 보기위한것
+    Transform start;
     Transform dest;
     Transform movedest;
     bool IsStart = false;
@@ -15,6 +16,7 @@ public class LockonLaserMotion : MonoBehaviour {
 
     private void Start()
     {
+        start = GameObject.Find("start").transform;
         dest = GameObject.Find("dest").transform;
         
         int a = 0;
@@ -28,8 +30,14 @@ public class LockonLaserMotion : MonoBehaviour {
         // 최종 목적지에서 시작 위치의 벡터에서 일정량만큼 좌우로 벌어진 위치 생성
         //Vector3 guideline = 
 
-        tr = (Transform)Instantiate(guidePrefab, dest.position, dest.rotation);
+        tr = (Transform)Instantiate(guidePrefab,
+                                    start.position,
+                                    start.rotation);
 
+        tr.position = new Vector3(start.position.x + 1, 
+                                  start.position.y, 
+                                  start.position.z);
+        
         return tr;
     }
 
@@ -39,14 +47,14 @@ public class LockonLaserMotion : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.A))
         {
             IsStart = true;
-            dest = CrateDest();
+            movedest = CrateDest();
         }
 
         if (IsStart)
         {
             //transform.position = Lerp(transform.position, dest.position, speed * Time.deltaTime);
             
-            transform.Translate(dest.position * speed * Time.deltaTime, Space.World);
+            transform.Translate(movedest.position * speed * Time.deltaTime, Space.World);
 
             //Vector3 v = transform.position - dest.position;
         }
