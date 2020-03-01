d29db31b36f80f2e2865164870742ea266adac47
DESKTOP-4TAOHB8\hunter <hunter10@naver.com>
2020-03-01 16:58:07 +0900

    Update Unity2018.4.13f1

--- Assets/Scripts/mover.cs
+++ Assets/Scripts/mover.cs
@@ -9,20 +9,42 @@ public class mover : MonoBehaviour {
     float speed = 1f;
     public Transform dest;
     bool IsStart = false;
+
+    float delta = 0;
 	void Update () {
         if(Input.GetKeyDown(KeyCode.A))
         {
             IsStart = true;
+            //transform.position = transform.position + (dest.position - transform.position);
+            //delta = 1f;
         }
 
-        if(IsStart)
-            transform.position = Lerp(transform.position, dest.position, speed * Time.deltaTime);    	
+        if (IsStart)
+        {
+            // 1. 해당시간안에 정속도로 움직이기
+            //delta += Time.deltaTime;
+            //float percent = delta / speed;
+            //if (percent > 1f)
+            //    percent = 1f;
+
+            // 2. 목표에 가까울수록 느려지기
+            //float percent = Time.deltaTime;
+
+            // 3. 목표에 가까울수록 빨라지기(최초에 극히 작은수에서 점점 커지는 함수를 쓰면됨)
+            delta += Time.deltaTime;
+            float percent = delta;
+
+            transform.position = Lerp(transform.position, dest.position, percent);
+        }
 	}
 
     // Step1
     Vector3 Lerp(Vector3 start, Vector3 dest, float t)
     {
-        return (1f - t) * start + t * dest;
+        // 여기서 1은 전체 시간의 흐름속에서 100%라는 개념으로 봐야 함.
+        Vector3 result = ((1f - t) * start) + (t * dest);
+
+        return result;
     }
     
 
